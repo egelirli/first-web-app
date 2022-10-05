9d6cb915ef81f77bff0bb25836b2b7f856904fb4
egelirli <egelirli@havelsan.com.tr>
2022-10-05 19:49:29 +0300

    dsadasdas

--- src/main/java/com/egelirli/springmvc/LoginController.java
+++ src/main/java/com/egelirli/springmvc/LoginController.java
@@ -1,8 +1,10 @@
 package com.egelirli.springmvc;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -10,12 +12,17 @@ public class LoginController {
 
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	//@ResponseBody
-	public String sayHello() {
+	public String showLoginPage() {
 		 return "login";
 	}
 
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	public String handleLoginRequest(@RequestParam String name, ModelMap model) {
+		 model.put("name", name);
+		 return "welcome";
+	}	 
+	
 	@RequestMapping(value = "/test")
-	//@ResponseBody
 	public String test() {
 		 return "test";
 	}
--- src/main/webapp/WEB-INF/views/login.jsp
+++ src/main/webapp/WEB-INF/views/login.jsp
@@ -9,7 +9,7 @@
 </head>
 <body>
 
-<form action="/login.do" method="post">
+<form action="/login" method="post">
 <p> <font color="red"> ${errorMessage} </font> 
 </p>
 Enter your name <input type="text" name="name">  Password: <input type="password" name="password">  <input type="submit" value="Login" > 
